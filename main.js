ca31bf525550d96e66522442aacd531c26bc0e1b
Melissa Montesinos <melissa.d.montesinos@gmail.com>
2024-10-20 23:39:19 -0400

    Handle stock availability and disable purchase button when out of stock

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 document.addEventListener('DOMContentLoaded', () => { //Reference to the size selector and product price 
     const sizeSelector = document.getElementById('size-selector'); 
     const productPrice = document.getElementById('product-price');
-    const purchasebutton = document.getElementById('purchase-button'); // Task 3: Handle Stock Availability
+    const purchasebutton = document.getElementById('purchase-button'); // Handle Stock Availability
 
     const prices = { // Defining prices for sizes
         small: 8.00,
@@ -11,13 +11,12 @@ document.addEventListener('DOMContentLoaded', () => { //Reference to the size se
         large: 12.00,
     };
 
-    //Used add event listener to update price when size is selected
-    sizeSelector.addEventListener('change', (event) => {
+    sizeSelector.addEventListener('change', (event) => {  //Add event listener to show price of chosen size
         const selectedSize = event.target.value;
-        const product = stock[selectedSize]; // Task 3: Used to call stock data
+        const product = stock[selectedSize]; // Call data
         productPrice.textContent = `$${prices[selectedSize].toFixed(2)}`; 
 
-        if (product.available) { //If else function used to identify when to disable purchase button
+        if (product.available) { //If else to know when to disable the purchase button
             purchasebutton.disabled = false;
         } else {
             purchasebutton.disabled = true;
@@ -26,3 +25,10 @@ document.addEventListener('DOMContentLoaded', () => { //Reference to the size se
     });
 });
 
+// Task 3: Handle Stock Availability
+
+const stock = { // Define stock
+    small: {price: 8.00, available: true}, 
+    medium: {price: 10.00, available: true}, 
+    large: {price: 12.00, available: false},
+ };
